acc694dee2c92635c97e6f7abeaf40689edfe7d0
Hamza EL MANOUZI <hamzaelma96@gmail.com>
2025-03-07 08:31:02 +0000

    Update Main.java

--- Java-Practice/CompteBancaire/Main.java
+++ Java-Practice/CompteBancaire/Main.java
@@ -1,22 +1,22 @@
 package CompteBancaire;
 public class Main {
     public static void main(String[] args) {
-        // Création d'un compte bancaire avec un solde initial de 100€
+        // Création d'un compte bancaire avec un solde de 100 MAD
         CompteBancaire compte = new CompteBancaire(100);
 
-        // Affichage du solde initial
+        // Affichage du solde :
         System.out.println("Solde initial : " + compte.getSolde() + "MAD");
 
-        // Déposer 50€
+        // Déposer 50 MAD :
         compte.deposer(50);
 
-        // Essayer de retirer 30€
+        // Retirer 30 MAD :
         compte.retirer(30);
 
-        // Essayer de retirer 150€ (doit échouer)
+        // Essayer de retirer 150 MAD :
         compte.retirer(150);
 
-        // Affichage du solde final
+        // Affichage du solde final :
         System.out.println("Solde final : " + compte.getSolde() + "MAD");
     }
 }
