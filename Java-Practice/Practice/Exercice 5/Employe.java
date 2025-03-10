befe76daceff6323505d43df2207776115d46f0c
mxrtixn <hamzaelma96@gmail.com>
2025-03-10 15:27:01 +0000

    Practicing JAVA

---
+++ Java-Practice/Practice/Exercice 5/Employe.java
@@ -0,0 +1,18 @@
+public class Employe {
+    private String nom;
+    private int matricule;
+    private double indiceSalarial;
+
+
+    public Employe(String nom, int matricule, double indiceSalarial){
+        this.nom = nom;
+        this.matricule = matricule;
+        this.indiceSalarial = indiceSalarial;
+    }
+    public afficheCaracteristiques();
+
+    public calculerSalaire(){
+
+        
+    }
+}
