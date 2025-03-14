ad157c2b0b4ee2ead9c35b6c9c83c6c551df6ca5
mxrtixn <hamzaelma96@gmail.com>
2025-03-14 13:44:11 +0000

    Practicing JAVA

--- Java-Practice/Practice/Exercice 6/Ingenieur.java
+++ Java-Practice/Practice/Exercice 6/Ingenieur.java
@@ -1,7 +1,7 @@
 public class Ingenieur extends EmployeBase {
-    private static final double SALAIRE_FIXE = 9000;
-    private static final int SEUIL_HEURES = 180;
-    private static final double TARIF_HEURE_SUPP = 200;
+    private static  double SALAIRE_FIXE = 9000;
+    private static int SEUIL_HEURES = 180;
+    private static  double TARIF_HEURE_SUPP = 200;
 
     public Ingenieur(String nom, String prenom, String identifiant, int heuresTravaillees) {
         super(nom, prenom, identifiant, heuresTravaillees);
--- Java-Practice/Practice/Exercice 6/Technicien.java
+++ Java-Practice/Practice/Exercice 6/Technicien.java
@@ -1,8 +1,8 @@
 public class Technicien extends EmployeBase {
-    private static final double SALAIRE_FIXE = 3500;
-    private static final double PRIME = 2000;
-    private static final int SEUIL_HEURES = 200;
-    private static final double TARIF_HEURE_SUPP = 100;
+    private static double SALAIRE_FIXE = 3500;
+    private static double PRIME = 2000;
+    private static int SEUIL_HEURES = 200;
+    private static double TARIF_HEURE_SUPP = 100;
 
     public Technicien(String nom, String prenom, String identifiant, int heuresTravaillees) {
         super(nom, prenom, identifiant, heuresTravaillees);
