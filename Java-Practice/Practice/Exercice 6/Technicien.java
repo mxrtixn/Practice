a821acc8dbce1fda50e4f144b437fbd8aac4de3a
mxrtixn <hamzaelma96@gmail.com>
2025-03-14 11:32:32 +0000

    Practicing JAVA

--- Java-Practice/Practice/Exercice 6/Technicien.java
+++ Java-Practice/Practice/Exercice 6/Technicien.java
@@ -1,2 +1,16 @@
-public class Technicien {
+public class Technicien extends EmployeBase {
+    private static final double SALAIRE_FIXE = 3500;
+    private static final double PRIME = 2000;
+    private static final int SEUIL_HEURES = 200;
+    private static final double TARIF_HEURE_SUPP = 100;
+
+    public Technicien(String nom, String prenom, String identifiant, int heuresTravaillees) {
+        super(nom, prenom, identifiant, heuresTravaillees);
+    }
+
+    @Override
+    public double calculerSalaire() {
+        int heuresSupp = Math.max(0, heuresTravaillees - SEUIL_HEURES);
+        return SALAIRE_FIXE + PRIME + (heuresSupp * TARIF_HEURE_SUPP);
+    }
 }
